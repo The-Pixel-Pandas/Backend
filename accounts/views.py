85ec85a96fdc4975d1294f0d09be6fd4dade54d3
ali bahri <ali_hosseini80@comp.iust.ac.ir>
2025-04-06 15:49:49 +0330

    Init project

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from accounts.serializer import UserSerializer
+from rest_framework.permissions import AllowAny
+
+User = get_user_model()
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  # اجازه دسترسی بدون احراز هویت
+    
+    def perform_create(self, serializer):
+        user = serializer.save()
+        # ایجاد توکن برای کاربر جدید
+        Token.objects.create(user=user)
